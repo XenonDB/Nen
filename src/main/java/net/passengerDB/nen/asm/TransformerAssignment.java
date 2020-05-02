7331315354702746fd4e76918fb897afa84dfaef
XenonDB <58476103+XenonDB@users.noreply.github.com>
2020-05-02 16:28:59 +0800

    Add files via upload

--- src/main/java/net/passengerDB/nen/asm/TransformerAssignment.java
+++ src/main/java/net/passengerDB/nen/asm/TransformerAssignment.java
@@ -1,8 +1,7 @@
 package net.passengerDB.nen.asm;
 
 import net.minecraft.launchwrapper.IClassTransformer;
-import net.passengerDB.nen.asm.transformer.PreventBoatBlockedByPart;
-import net.passengerDB.nen.asm.transformer.raytrace.PlayerRayTraceExclude;
+import net.passengerDB.nen.asm.transformer.raytrace.WorldAABBexcluding;
 import net.passengerDB.nen.asm.transformer.*;
 
 public class TransformerAssignment implements IClassTransformer {
@@ -11,14 +10,12 @@ public class TransformerAssignment implements IClassTransformer {
 	public byte[] transform(String name, String transformedName, byte[] clsdata) {
 		
 		switch(transformedName) {
-		case "net.minecraft.client.renderer.EntityRenderer":
-			return PlayerRayTraceExclude.transform(name, transformedName, clsdata);
+		case "net.minecraft.world.World":
+			return WorldAABBexcluding.transform(name, transformedName, clsdata);
 		case "net.minecraft.entity.player.EntityPlayer"://attackTargetEntityWithCurrentItem
 			return EnchantmentResponse.transformForPlayer(name, transformedName, clsdata);
 		case "net.minecraft.entity.monster.EntityMob"://attackEntityAsMob
 			return EnchantmentResponse.transformForMob(name, transformedName, clsdata);
-		case "net.minecraft.item.ItemBoat":
-			return PreventBoatBlockedByPart.transform(name, transformedName, clsdata);
 		default:
 			return clsdata;
 		}
--- src/main/java/net/passengerDB/nen/asm/transformer/EnchantmentResponse.java
+++ src/main/java/net/passengerDB/nen/asm/transformer/EnchantmentResponse.java
@@ -28,40 +28,33 @@ public class EnchantmentResponse {
 	    MethodNode mn = ASMUtils.findFirstMethod(clsNode, name, func);
 	    
 	    InsnList insns = new InsnList();
-        try {
-        	AbstractInsnNode target = ASMUtils.findNthVarInsn(mn,Opcodes.FSTORE,3,2,false);
-        	AbstractInsnNode target2 = ASMUtils.findNthMethodInsn(mn, "net/minecraft/entity/player/EntityPlayer", "func_184614_ca", "()Lnet/minecraft/item/ItemStack;", 3, false);
-        	//讓不死剋星及節肢剋星可以根據宿主生效
-        	insns.add(new VarInsnNode(Opcodes.ALOAD,0));
-        	insns.add(new VarInsnNode(Opcodes.ALOAD,1));
-        	insns.add(new VarInsnNode(Opcodes.FLOAD,3));
-            insns.add(new MethodInsnNode(Opcodes.INVOKESTATIC, ASMUtils.getInsertMethodCls(), "applyEnchantmentToPartForPlayer", "(Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/entity/Entity;F)F", false));
-            insns.add(new VarInsnNode(Opcodes.FSTORE,3));
-            
-            mn.instructions.insert(target, insns);
-            
-            //使itemstack1.hitEntity及火焰附加可以生效
-            insns = new InsnList();
-            
-            insns.add(new VarInsnNode(Opcodes.ALOAD,0));
-            insns.add(new VarInsnNode(Opcodes.ALOAD,1));
-            insns.add(new MethodInsnNode(Opcodes.INVOKESTATIC, ASMUtils.getInsertMethodCls(), "attackHandlerForPlayer", "(Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/entity/Entity;)V", false));
-            
-            mn.instructions.insert(target2, insns);
-            
-            ClassWriter writer = new ClassWriter(0);
-            clsNode.accept(writer);
-            
-            ASMUtils.info("Successfully transform class EntityPlayer.");
-            
-            return writer.toByteArray();
-        }
-        catch(Exception e) {
-        	ASMUtils.error("Something went wrong. Print the stack trace...");
-        	e.printStackTrace();
-        }
-	    
-		return clsdata;
+        
+	    AbstractInsnNode target = ASMUtils.findNthVarInsn(mn,Opcodes.FSTORE,3,2,false);
+    	AbstractInsnNode target2 = ASMUtils.findNthMethodInsn(mn, "net/minecraft/entity/player/EntityPlayer", "func_184614_ca", "()Lnet/minecraft/item/ItemStack;", 3, false);
+    	//讓不死剋星及節肢剋星可以根據宿主生效
+    	insns.add(new VarInsnNode(Opcodes.ALOAD,0));
+    	insns.add(new VarInsnNode(Opcodes.ALOAD,1));
+    	insns.add(new VarInsnNode(Opcodes.FLOAD,3));
+        insns.add(new MethodInsnNode(Opcodes.INVOKESTATIC, ASMUtils.getInsertMethodCls(), "applyEnchantmentToPartForPlayer", "(Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/entity/Entity;F)F", false));
+        insns.add(new VarInsnNode(Opcodes.FSTORE,3));
+        
+        mn.instructions.insert(target, insns);
+        
+        //使itemstack1.hitEntity及火焰附加可以生效
+        insns = new InsnList();
+        
+        insns.add(new VarInsnNode(Opcodes.ALOAD,0));
+        insns.add(new VarInsnNode(Opcodes.ALOAD,1));
+        insns.add(new MethodInsnNode(Opcodes.INVOKESTATIC, ASMUtils.getInsertMethodCls(), "attackHandlerForPlayer", "(Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/entity/Entity;)V", false));
+        
+        mn.instructions.insert(target2, insns);
+        
+        ClassWriter writer = new ClassWriter(0);
+        clsNode.accept(writer);
+        
+        ASMUtils.info(String.format("Successfully transform class %s.", transformedName));
+        
+        return writer.toByteArray();
 	}
 	
 	public static byte[] transformForMob(String name, String transformedName, byte[] clsdata) {
@@ -75,40 +68,34 @@ public class EnchantmentResponse {
 	    MethodNode mn = ASMUtils.findFirstMethod(clsNode, name, func);
 	    
 	    InsnList insns = new InsnList();
-        try {
-        	AbstractInsnNode target = ASMUtils.findNthVarInsn(mn,Opcodes.ALOAD,1,1,false);
-        	AbstractInsnNode target2 = ASMUtils.findNthMethodInsn(mn,"net/minecraft/entity/monster/EntityMob","func_174815_a","(Lnet/minecraft/entity/EntityLivingBase;Lnet/minecraft/entity/Entity;)V",1,true);
-        	//
-        	insns.add(new VarInsnNode(Opcodes.ALOAD,0));
-        	insns.add(new VarInsnNode(Opcodes.ALOAD,1));
-        	insns.add(new VarInsnNode(Opcodes.FLOAD,2));
-            insns.add(new MethodInsnNode(Opcodes.INVOKESTATIC, ASMUtils.getInsertMethodCls(), "applyEnchantmentToPartForMob", "(Lnet/minecraft/entity/monster/EntityMob;Lnet/minecraft/entity/Entity;F)F", false));
-            insns.add(new VarInsnNode(Opcodes.FSTORE,2));
-            
-            mn.instructions.insertBefore(target, insns);
-            
-            //
-            insns = new InsnList();
-            
-            insns.add(new VarInsnNode(Opcodes.ALOAD,0));
-        	insns.add(new VarInsnNode(Opcodes.ALOAD,1));
-            insns.add(new MethodInsnNode(Opcodes.INVOKESTATIC, ASMUtils.getInsertMethodCls(), "attackHandlerForMob", "(Lnet/minecraft/entity/monster/EntityMob;Lnet/minecraft/entity/Entity;)V", false));
-            
-            mn.instructions.insert(target2, insns);
-            
-            ClassWriter writer = new ClassWriter(0);
-            clsNode.accept(writer);
-            
-            ASMUtils.info("Successfully transform class EntityMob.");
-            
-            return writer.toByteArray();
-        }
-        catch(Exception e) {
-        	ASMUtils.error("Something went wrong. Print the stack trace...");
-        	e.printStackTrace();
-        }
 	    
-		return clsdata;
+	    AbstractInsnNode target = ASMUtils.findNthVarInsn(mn,Opcodes.ALOAD,1,1,false);
+    	AbstractInsnNode target2 = ASMUtils.findNthMethodInsn(mn,"net/minecraft/entity/monster/EntityMob","func_174815_a","(Lnet/minecraft/entity/EntityLivingBase;Lnet/minecraft/entity/Entity;)V",1,true);
+    	
+    	//讓怪物攻擊到part後的附魔可以根據宿主生效(問題是有怪物會瞄準它嗎?)
+    	insns.add(new VarInsnNode(Opcodes.ALOAD,0));
+    	insns.add(new VarInsnNode(Opcodes.ALOAD,1));
+    	insns.add(new VarInsnNode(Opcodes.FLOAD,2));
+        insns.add(new MethodInsnNode(Opcodes.INVOKESTATIC, ASMUtils.getInsertMethodCls(), "applyEnchantmentToPartForMob", "(Lnet/minecraft/entity/monster/EntityMob;Lnet/minecraft/entity/Entity;F)F", false));
+        insns.add(new VarInsnNode(Opcodes.FSTORE,2));
+        
+        mn.instructions.insertBefore(target, insns);
+        
+        //使部分怪物攻擊成功後運算能生效
+        insns = new InsnList();
+        
+        insns.add(new VarInsnNode(Opcodes.ALOAD,0));
+    	insns.add(new VarInsnNode(Opcodes.ALOAD,1));
+        insns.add(new MethodInsnNode(Opcodes.INVOKESTATIC, ASMUtils.getInsertMethodCls(), "attackHandlerForMob", "(Lnet/minecraft/entity/monster/EntityMob;Lnet/minecraft/entity/Entity;)V", false));
+        
+        mn.instructions.insert(target2, insns);
+        
+        ClassWriter writer = new ClassWriter(0);
+        clsNode.accept(writer);
+        
+        ASMUtils.info(String.format("Successfully transform class %s.", transformedName));
+        
+        return writer.toByteArray();
 	}
 
 	
